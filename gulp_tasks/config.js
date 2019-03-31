330e8cd45a1b741a76ae9377631385d1c7f968cc
Potcharapol Suteparuk <psuteparuk@gmail.com>
2019-03-30 20:03:24 -0700

    Remove Gulp and switch to npm scripts

--- gulp_tasks/config.js
+++
@@ -1,8 +0,0 @@
-const path = require('path');
-
-exports.IS_PROD = process.env.ENV === 'production';
-
-exports.path = {
-  sourceDir: path.resolve(__dirname, '..', 'src'),
-  staticDir: path.resolve(__dirname, '..', 'static')
-}
--- gulpfile.js
+++
@@ -1,6 +0,0 @@
-const gulp = require('gulp');
-const requireDir = require('require-dir');
-
-requireDir('./gulp_tasks');
-
-gulp.task('default', ['watch:css']);
